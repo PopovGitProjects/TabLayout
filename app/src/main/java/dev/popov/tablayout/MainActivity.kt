e049671b7dcc41a402f7f3e40402f97284de15da
Sergey Popov <popov.workmail@gmail.com>
2022-04-21 17:46:11 +0500

    добавил слушатель прелистываний

--- app/src/main/java/dev/popov/tablayout/MainActivity.kt
+++ app/src/main/java/dev/popov/tablayout/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.popov.tablayout
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import dev.popov.tablayout.adapter.ViewPagerAdapter
 import dev.popov.tablayout.databinding.ActivityMainBinding
@@ -28,12 +29,27 @@ class MainActivity : AppCompatActivity() {
                 1 -> {
                     tab.setIcon(R.drawable.settings)
                     tab.icon?.setTint(ContextCompat.getColor(this@MainActivity, R.color.two))
+                    tab.icon?.alpha = 70
                 }
                 else -> {
                     tab.setIcon(R.drawable.backup)
                     tab.icon?.setTint(ContextCompat.getColor(this@MainActivity, R.color.three))
+                    tab.icon?.alpha = 70
                 }
         }
         }.attach()
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.icon?.alpha = 250
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                tab?.icon?.alpha = 70
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
     }
 }
